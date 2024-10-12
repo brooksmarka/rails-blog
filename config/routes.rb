569bcf0f5ffc3061d0097ab9ddf7ac3d07e17cc1
Mark Brooks <markabrooks@gmail.com>
2024-10-12 17:23:00 -0600

    add ability to submit form from articles/new

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
+	# Resources
+	resources :articles
+
   # Defines the root path route ("/")
   root "welcome#index"
 end
